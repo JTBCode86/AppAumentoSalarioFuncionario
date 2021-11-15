168695b484b2afe722c25cae5bb6646172e2788b
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-11-14 21:34:30 -0300

    Adicionar arquivos de projeto.

---
+++ Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace AppAumentoSalarioFuncionario
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salarioBruto;
+        public double imposto;
+
+        public double salarioLiquido() 
+        {
+            return salarioBruto - imposto;
+        }       
+
+        public void aumentarSalario(double percentual) 
+        {
+            salarioBruto = salarioBruto + (salarioBruto * percentual / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return
+                nome
+                + ", R$ "
+                + salarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AppAumentoSalarioFuncionario
+{
+    /// <summary>
+    /// Exercicios - Fixação - POO - Classes
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            
+            Console.WriteLine("Nome: ");
+            funcionario.nome = Console.ReadLine();
+            
+            Console.WriteLine("Salario Bruto: ");
+            funcionario.salarioBruto = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Imposto: ");
+            funcionario.imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionario "+ funcionario);
+
+            Console.WriteLine();
+            Console.WriteLine("Deseja aumentar o salario em qual percentual?");
+            double percentual = double.Parse(Console.ReadLine());
+            funcionario.aumentarSalario(percentual);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: "+ funcionario);
+            Console.ReadLine();
+        }
+    }
+}
